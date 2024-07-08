4056cdd928e5dd30c603ea3579c6592401e5f404
Muhammadqodir Alijonov <alijonov0608@gmail.com>
2024-07-09 00:40:01 +0500

    add dockerfile

---
+++ src/ICollection.DataAccess/Migrations/20240708103624_4comment.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ICollection.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class _4comment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
